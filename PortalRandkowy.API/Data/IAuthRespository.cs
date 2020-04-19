620a73005da7e175e53e499cec06f7a01a62078b
RafałRadzik <40192818+Rafal97radzik@users.noreply.github.com>
2020-04-19 14:44:19 +0200

    Utworzenie Interfejsu do autoryzacji

---
+++ PortalRandkowy.API/Data/IAuthRespository.cs
@@ -0,0 +1,17 @@
+﻿using PortalRandkowy.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalRandkowy.API.Data
+{
+    public interface IAuthRespository
+    {
+        Task<User> Login(string username, string password);
+
+        Task<User> Register(User user, string password);
+
+        Task<bool> UserExists(string username);
+    }
+}
