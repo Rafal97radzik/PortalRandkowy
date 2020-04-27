fa6813325c356c297b9b26a3e56b932bd9ef1cdc
RafałRadzik <40192818+Rafal97radzik@users.noreply.github.com>
2020-04-27 19:40:40 +0200

    Dodanie metody rejestracji

--- PortalRandkowy.SPA/src/app/_services/auth.service.ts
+++ PortalRandkowy.SPA/src/app/_services/auth.service.ts
@@ -1,24 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-baseUrl = 'https://localhost:44390/api/auth/';
+  baseUrl = 'https://localhost:44390/api/auth/';
 
-constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-login(model: any){
-return this.http.post(this.baseUrl + 'login', model)
-.pipe(map((response: any) => {
-  const user = response;
-  if (user){
-    localStorage.setItem('token', user.token);
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model)
+      .pipe(map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('token', user.token);
+        }
+      }));
   }
-}));
+
+register(model: any){
+  return this.http.post(this.baseUrl + 'register', model);
 }
 
 }
