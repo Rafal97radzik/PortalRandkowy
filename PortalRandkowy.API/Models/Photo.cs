c0f6d6b694eff6374c59c3eb8bb539923ffd4a4b
RafałRadzik <40192818+Rafal97radzik@users.noreply.github.com>
2020-05-02 21:00:45 +0200

    Aktualizacja modelu użytkownika

---
+++ PortalRandkowy.API/Models/Photo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalRandkowy.API.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Descriptio { get; set; }
+        public DateTime DateAdded { get; set; }
+        public bool IsMain { get; set; }
+    }
+}
