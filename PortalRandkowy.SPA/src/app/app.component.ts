d37478a848033288cf6f135a566f6ffd690cfd22
RafałRadzik <40192818+Rafal97radzik@users.noreply.github.com>
2020-05-08 19:35:37 +0200

    Dodanie bootswatch

--- PortalRandkowy.SPA/src/app/app.component.ts
+++ PortalRandkowy.SPA/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './_services/auth.service';
-import {JwtHelperService} from '@auth0/angular-jwt';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +11,12 @@ export class AppComponent implements OnInit {
 
   jwtHelper = new JwtHelperService();
 
-constructor(private authSerices: AuthService){}
+  constructor(private authSerices: AuthService) { }
 
   ngOnInit(): void {
     const token = localStorage.getItem('token');
     if (token) {
-this.authSerices.decodedToken = this.jwtHelper.decodeToken(token);
+      this.authSerices.decodedToken = this.jwtHelper.decodeToken(token);
     }
   }
 }
