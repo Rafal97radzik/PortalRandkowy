f9c1747e1d74df4c9263f7224cba767bc3150aee
RafałRadzik <40192818+Rafal97radzik@users.noreply.github.com>
2020-06-15 20:14:29 +0200

    Dodanie Pagination Helper

---
+++ PortalRandkowy.API/Helpers/UserParams.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalRandkowy.API.Helpers
+{
+    public class UserParams
+    {
+        public const int MaxPageSize = 48;
+        int pageSize=24;
+        
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get=>pageSize; set=>pageSize=Math.Min(MaxPageSize, value); }
+    }
+}
